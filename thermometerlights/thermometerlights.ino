d27d470a92c9ca39bae776eef19a8feb4736bfb6
caitlinsdad <imanage@gmail.com>
2016-01-13 09:12:40 -0500

    Arduino
    sketches

---
+++ thermometerlights/thermometerlights.ino
@@ -0,0 +1,245 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+// Strandtest neopixel code from Adafruit
+
+#include "DHT.h"
+#include <Adafruit_NeoPixel.h>
+
+#define DHTPIN 10     // what digital pin we're connected to
+#define PIXEL_PIN    6    // Digital IO pin connected to the NeoPixels.
+#define PIXEL_COUNT 20
+
+#define RPIXEL_PIN   9    // Digital IO pin connected to the NeoPixel Ring.
+#define RPIXEL_COUNT 16
+
+// Parameter 1 = number of pixels in strip,  neopixel stick has 8
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream
+//   NEO_GRB     Pixels are wired for GRB bitstream, correct for neopixel stick
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip), correct for neopixel stick
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel Rstrip = Adafruit_NeoPixel(RPIXEL_COUNT, RPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+// Uncomment whatever type you're using!
+  #define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+int y; // define global var for number of pixels
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+
+  Rstrip.begin();
+  Rstrip.show(); // Initialize all pixels to 'off'
+ 
+  dht.begin();
+
+y =strip.numPixels();
+
+ RcolorWipe(Rstrip.Color(255, 0, 0), 150);  // turn on Red ring
+ startShow(7); // rainbow color wipe - test all neopixels
+ startShow(11); // fill all to red
+  startShow(10); //reverse fill to blank
+
+}
+ 
+
+void loop() {
+  // Wait a few seconds between measurements.
+  
+  delay(2000);
+  
+ 
+   
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.print(" *F ");
+  
+y = map(f,0,100,0,20);
+  Serial.print(y);
+  Serial.println(" scale");
+  
+     startShow(10); //reverse fill to blank
+     startShow(1); // fill up
+     startShow(5); // theaterchase
+     startShow(11); // fast fill up
+     
+}
+
+void startShow(int i) {
+  switch(i){
+    case 0: colorWipe(strip.Color(0, 0, 0), 50);    // Black/off
+            break;
+    case 1: colorWipe(strip.Color(255, 0, 0), 150);  // Red
+            break;
+    case 2: colorWipe(strip.Color(0, 255, 0), 50);  // Green
+            break;
+    case 3: colorWipe(strip.Color(0, 0, 255), 50);  // Blue
+            break;
+    case 4: theaterChase(strip.Color(127, 127, 127), 50); // White
+            break;
+    case 5: theaterChase(strip.Color(127,   0,   0), 120); // Red
+            break;
+    case 6: theaterChase(strip.Color(  0,   0, 127), 50); // Blue
+            break;
+    case 7: rainbow(20);
+            break;
+    case 8: rainbowCycle(20);
+            break;
+    case 9: theaterChaseRainbow(50);
+            break;
+    case 10: colorWipeX(strip.Color(0, 0, 0), 120);    // Black/off
+            break;
+    case 11: colorWipe(strip.Color(255, 0, 0), 10);  // Red
+            break;
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipeX(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<y+1; i++) {
+    strip.setPixelColor(y-i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<y; i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void RcolorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<16; i++) {
+    Rstrip.setPixelColor(i, c);
+    Rstrip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<y; i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<40; j++) {  //do 40 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < y; i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i=0; i < y; i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
